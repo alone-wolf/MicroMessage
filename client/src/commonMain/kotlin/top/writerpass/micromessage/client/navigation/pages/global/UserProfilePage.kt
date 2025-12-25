b20d9838c8a46fba2303623e387323df6c24b1d0
WriterPass <mail@writerpass.top>
2025-12-25 17:07:49 +0800

    update

--- client/src/commonMain/kotlin/top/writerpass/micromessage/client/navigation/pages/global/UserProfilePage.kt
+++ client/src/commonMain/kotlin/top/writerpass/micromessage/client/navigation/pages/global/UserProfilePage.kt
@@ -1,12 +1,18 @@
 package top.writerpass.micromessage.client.navigation.pages.global
 
 import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowRight
 import androidx.compose.runtime.Composable
@@ -16,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NamedNavArgument
@@ -24,6 +32,7 @@ import androidx.navigation.NavType
 import androidx.navigation.navArgument
 import androidx.savedstate.read
 import top.writerpass.cmplibrary.LaunchedEffectOdd
+import top.writerpass.cmplibrary.compose.FullSizeColumn
 import top.writerpass.cmplibrary.compose.FullWidthRow
 import top.writerpass.cmplibrary.compose.ables.IconComposeExt.CxIcon
 import top.writerpass.cmplibrary.compose.ables.TextComposeExt.CxText
@@ -50,7 +59,33 @@ object UserProfilePage : IPage {
                     userId = getLong("userId")
                 }
             }
-            "UserId: ${userId}".CxText()
+            FullSizeColumn {
+                "UserId: ${userId}".CxText()
+                FullWidthRow(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(Color.DarkGray)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Column {
+                        "Remark".CxText()
+                        "User's Nickname".CxText()
+                        "User's Id".CxText()
+                        "Location Beijing".CxText()
+                    }
+                }
+                FullWidthRow(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                }
+            }
 
             fun LazyListScope.infoItem(
                 label: String,
